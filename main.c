c7c80afa97a9fccf75ab1b64afa396b152552a4e
mauriciolasgon <108753106+mauriciolasgon@users.noreply.github.com>
2023-11-22 15:09:15 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,31 +2,34 @@
 #include <stdlib.h>
 #include "AlgoritimosOrdenacao.h"
 #include <time.h>
+#include <math.h>
 
 int main()
 {
     vetor **vet=NULL;
-    int tam=pow(10,5);
+    int tam=pow(10,4);
+    double media=0;
     clock_t inicio,fim;
     double tempDecorrido;
 
     vet=geradorDeVetorAleatorio(vet,tam);
-    //MergeSort(x,20);
 
 
     printf("\n");
     for(int i=0;i<10;i++){
         inicio=clock();
-        ShellSort(vet[i],tam);
+        InsertionSort(vet[i],tam);
         fim=clock();
         tempDecorrido=((double) (fim-inicio))/CLOCKS_PER_SEC ;
+        media+=tempDecorrido;
         printf("Vetor %d: %f s \n",i+1,tempDecorrido );
     }
+    printf("\nTempo medio: %f s", media/10);
     printf("\n");
 
 
 
     liberaVetor(vet);
-    //liberaVetor(vet);
+
     return 0;
 }
